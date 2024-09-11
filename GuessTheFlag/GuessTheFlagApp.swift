6e98464b81b6880fd748565d3f623f4ca6529e85
phil002109 <vermontphil.paguiligan@gmail.com>
2024-09-11 12:53:39 +0800

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vermont Phil Paguiligan on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
